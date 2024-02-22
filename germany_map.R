9f821c3688e293bc804437064579d71894b7d07b
Olga Miljukov <miljukov_o@ukw.de>
2024-02-22 15:13:57 +0100

    removes unnecessary map

--- germany_map.R
+++ germany_map.R
@@ -2,20 +2,20 @@ library(tidyverse)
 library(sf)
 library(ggspatial)
 
-germany <- read_sf("data/vg2500_12-31.utm32s.shape/vg2500/VG2500_LAN.shp")
-
-germany <- germany %>%
-  filter(GF != 8) %>%
-  select(GF, geometry)
+# germany <- read_sf("data/vg2500_12-31.utm32s.shape/vg2500/VG2500_LAN.shp")
+# 
+# germany <- germany %>%
+#   filter(GF != 8) %>%
+#   select(GF, geometry)
 
 germany_kreise <- read_sf("data/vg2500_12-31.utm32s.shape/vg2500/VG2500_KRS.shp")
 
 cities <- germany_kreise %>%
- # filter(str_detect(GEN, "Würz|Nürn")) %>%
-   select(geometry, GEN) %>%
+  #filter(str_detect(GEN, "Würz|Nürn")) %>%
+   select(geometry, GEN, AGS) %>%
   mutate(fictional_outcome = round(runif(nrow(.), 0, 100), 0))
 
 ggplot() +
-  ggplot2::geom_sf(data = germany, fill = NA, colour = "black") +
+  #ggplot2::geom_sf(data = germany, fill = NA, colour = "black") +
   ggplot2::geom_sf(data = cities, aes(fill = fictional_outcome)) +
   theme_void()
