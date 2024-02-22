8e04edca9f97059efe4a3a777aca703157261006
miljukov-o <77740463+miljukov-o@users.noreply.github.com>
2024-02-22 15:17:08 +0100

    Update germany_map.R

--- germany_map.R
+++ germany_map.R
@@ -12,12 +12,12 @@ library(ggspatial)
 
 germany_kreise <- read_sf("data/vg2500_12-31.utm32s.shape/vg2500/VG2500_KRS.shp")
 
-cities <- germany_kreise %>%
+germany_kreise <- germany_kreise %>%
   #filter(str_detect(GEN, "Würz|Nürn")) %>%
    select(geometry, GEN, AGS) %>%
   mutate(fictional_outcome = round(runif(nrow(.), 0, 100), 0))
 
 ggplot() +
   #ggplot2::geom_sf(data = germany, fill = NA, colour = "black") +
-  ggplot2::geom_sf(data = cities, aes(fill = fictional_outcome)) +
+  ggplot2::geom_sf(data = germany_kreise, aes(fill = fictional_outcome)) +
   theme_void()
